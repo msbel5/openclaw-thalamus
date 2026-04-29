8e3a47de9f58cbee0a1862b0762251207b4d1706
Muhammet Sıddık Bel <muhammet.siddikbel@gi-holding.com>
2026-04-29 21:11:02 +0300

    chore: scaffold typescript package

---
+++ eslint.config.js
@@ -0,0 +1,25 @@
+import js from "@eslint/js";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import tseslint from "typescript-eslint";
+
+const rootDir = path.dirname(fileURLToPath(import.meta.url));
+
+export default tseslint.config(
+  {
+    ignores: ["dist/**", "node_modules/**", "coverage/**"],
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  {
+    languageOptions: {
+      parserOptions: {
+        project: "./tsconfig.eslint.json",
+        tsconfigRootDir: rootDir,
+      },
+    },
+    rules: {
+      "@typescript-eslint/no-magic-numbers": "off",
+    },
+  },
+);
